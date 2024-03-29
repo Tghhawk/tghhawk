2afee61cbf49dd29929b6cc09ee24b398c94d71c
Tghhawk <151602119+Tghhawk@users.noreply.github.com>
2024-03-29 10:57:00 +0500

    Add files via upload

---
+++ api/script.js
@@ -0,0 +1,33 @@
+
+
+
+
+
+const wrapper = document.querySelector('.wrapper')
+
+
+
+const func = async () => {
+  const response = await fetch('https://dummyjson.com/users')
+
+  const details = await response.json()
+
+  console.log(details.users);
+
+  details.users.map((el)=>{
+    wrapper.innerHTML +=`
+    <div class="card">
+    <img src=${el.image} alt="">
+    <h2>${el.firstName} ${el.lastName}</h2>
+    <h3>${el.phone}</h3>
+    <p>${el.age}</p>
+    <b>${el.birthDate}</b>
+    <h4>${el.address.city}</h4>
+    </div>
+    `
+  })
+
+}
+
+
+func()
